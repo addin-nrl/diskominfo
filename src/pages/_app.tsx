81e5dda5480bfde00c15d8dcbfbacfbb9da30860
addin-nrl <addininrl221@gmail.com>
2023-06-03 17:55:47 +0700

    update: adding progress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import NextNProgress from "nextjs-progressbar";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextNProgress
+        color="#fff"
+        startPosition={0.1}
+        showOnShallow
+        options={{
+          easing: "ease",
+          speed: 1000,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
